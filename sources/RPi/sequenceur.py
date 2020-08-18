8d17cb8adf6b2b31939035cf18a7bffc328d27ae
Laurent <laurentremy0@gmail.com>
2020-08-18 22:28:55 +0200

    refactor + add usage of finder

--- sources/RPi/sequenceur.py
+++ sources/RPi/sequenceur.py
@@ -1,53 +1,35 @@
 import asyncio
-import pandas as pd
 import os
 import paho.mqtt.client as mqtt
+import finder
 
+# constants
+TOPIC_END = "end"
+TOPIC_SEARCH_BASE = "search"
+TOPIC_ERROR_NO_COMPONENTS = "error/no-components"
 
-def initialise_idents(csv_filename="idents.csv"):
-    return pd.read_csv(csv_filename, encoding='utf-8')
+find = finder()
 
-
-def idents_pour(id_produit):
-    DF_Idents = initialise_idents()
-    return list(DF_Idents["ident"][DF_Idents["ID objet"] == id_produit])
-
-
-def Scan2Ident(scanvalue):
-    "43487459001056660600010000001000"
-    if len(scanvalue) == 32:
-        return int(scanvalue[12:18])
-    else:
-        return None
 
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code " + str(rc))
 
-product_ident = None
 
 def on_message(client, userdata, message):
-    if message.payload == b"fin d'of":
-        client.publish(topic="/stat/end/{}".format(NomIlot), payload=str(product_ident), qos=1,retain=False)
-        client.publish(topic="/{}/off".format(NomIlot), payload="1", qos=0, retain=False)
-        product_ident = None
+    client.publish(topic=TOPIC_END, payload=message.payload,
+                   qos=1, retain=False)
+    etopic = find.explode_topic(message.topic)
+    components = find.search_components(etopic["info"])
+    if size(components) == 0:
+        client.publish(topic=TOPIC_ERROR_NO_COMPONENTS,
+                       payload=message.payload, qos=1, retain=False)
     else:
-        _id = Scan2Ident(message.payload)
-        if _id != None:
-            if _id == product_ident:
-                client.publish(topic="/{}/blink".format(NomIlot), payload="1", qos=0, retain=False)
-            else:
-                product_ident = _id
-                scan()
+        for component in components:
+            client.publish(topic=TOPIC_SEARCH_BASE + "/{}".format(component),
+                           payload=message.payload, qos=1, retain=False)
 
 
-def scan():
-    _idents = list(df_idents["ident"][df_idents["ID objet"] == product_ident])
-    client.publish(topic="/stat/start/{}/of/".format(NomIlot), payload="{}".format(product_ident), qos=0,
-                   retain=False)  # fait par scanner
-    for _id in _idents:
-        client.publish(topic="/{}/{}".format(NomIlot, _id), payload=b"0,0,0,100", qos=0, retain=False)
-
-def startClient(client):
+def start_client():
     client = mqtt.Client()
     client.on_connect = on_connect
     client.on_message = on_message
@@ -62,4 +44,5 @@ async def main():
 
 
 if __name__ == '__main__':
+    start_client
     asyncio.run(main())
