b2923ec7f0ddfbe8639482f28ddabf947c3cb851
Laurent <laurentremy0@gmail.com>
2020-08-23 21:51:57 +0200

    remove config reader

--- sources/ESP8266/demo/boot.py
+++ sources/ESP8266/demo/boot.py
@@ -34,21 +34,6 @@ def neo_write(t_color):
     leds.write()
 
 
-def get_config(config_file="config.yaml"):
-    # get the configuration dictionary from yaml file
-    f = open(config_file)
-    content = f.read()
-    f.close()
-    choix = content.split("\n")
-    config = {}
-    for kv in choix:
-        if kv != '':
-            key, val = kv.split(":")
-            key = key.strip()
-            config[key] = val.strip()
-    return config
-
-
 def do_connect(network, password):
     # connect to Wifi
     import network
