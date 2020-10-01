cd6a2a2826836e4c4b8141fde83b94976d77ce3d
saouinet <saouinet@googlemail.com>
2020-10-01 17:30:26 +0200

    Sync RPI version
    Just a synchronization with dev contents (ante 2020/09)

--- sources/RPi/scanner.py
+++ sources/RPi/scanner.py
@@ -1,82 +1,88 @@
 #!/usr/bin/python3
 
-import asyncio
 import evdev
-import paho.mqtt.client as mqtt
 from evdev import ecodes
+import asyncio
+import paho.mqtt.client as mqtt
 
-# constants
-TOPIC_END = "end"
-TOPIC_SEARCH_BASE = "search"
+client = mqtt.Client()
+client.connect("localhost", 1883)
+#client.connect("192.168.1.2", 1883)
+client.loop_start()
+
+r=0x10000
+v=0x100
+b=0x1
+couleurs={"couleur.blanc":100*r+100*v+100*b,
+"couleur.vert":0*r+100*v+0*b,
+"couleur.bleu":0*r+0*v+100*b,
+"couleur.rose":100*r+50*v+100*b}
+
 scancodes = {
     # Scancode: ASCIICode
     0: None, 1: u'ESC', 2: u'1', 3: u'2', 4: u'3', 5: u'4', 6: u'5', 7: u'6', 8: u'7', 9: u'8',
-    10: u'9', 11: u'0', 12: u'-', 13: u'=', 14: u'BKSP', 15: u'TAB', 16: u'q', 17: u'w', 18: u'e', 19: u'r',
+    10: u'9', 11: u'0', 12: u'-', 13: u'=', 14: u'BKSP', 15: u'TAB', 16: u'a', 17: u'w', 18: u'e', 19: u'r',
     20: u't', 21: u'y', 22: u'u', 23: u'i', 24: u'o', 25: u'p', 26: u'[', 27: u']', 28: u'CRLF', 29: u'LCTRL',
-    30: u'a', 31: u's', 32: u'd', 33: u'f', 34: u'g', 35: u'h', 36: u'j', 37: u'k', 38: u'l', 39: u';',
-    40: u'"', 41: u'`', 42: u'LSHFT', 43: u'\\', 44: u'z', 45: u'x', 46: u'c', 47: u'v', 48: u'b', 49: u'n',
-    50: u'm', 51: u',', 52: u'.', 53: u'/', 54: u'RSHFT', 56: u'LALT', 57: u' ', 100: u'RALT'
+    30: u'q', 31: u's', 32: u'd', 33: u'f', 34: u'g', 35: u'h', 36: u'j', 37: u'k', 38: u'l', 39: u'm',
+    40: u'%', 41: u'*', 42: u'LSHFT', 43: u'<', 44: u'w', 45: u'x', 46: u'c', 47: u'v', 48: u'b', 49: u'n',
+    50: u',', 51: u'.', 52: u':', 53: u'!', 54: u'RSHFT', 56: u'LALT', 57: u' ', 100: u'RALT'
 }
 
-
-# connexion (async) to mqtt broker
-client = mqtt.Client()
-client.connect("localhost", 1883)
-client.loop_start()
-
+scanners={}
 
 def getDevice():
-    # get the list of all scanning devices
+    print("looking for scanners ...",end="")
     devices = [evdev.InputDevice(path) for path in evdev.list_devices()]
     for d in devices:
-        print(d)
+        #print(d)
         if "Scanner" in d.name:
+            scanners[d.phys] = d
+            scanners[d.phys].couleur = couleurs["couleur.blanc"]
             yield d
-            print("found: {}".format(d))
+            #print("found: {}".format(d.phys))
+    print("finish")
 
+def envoyerFin(dev):
+    """ topic:end, payload:couleur courante """
+    client.publish(topic="end", payload="{}".format(scanners[dev].couleur), qos=0, retain=False)
+
+def envoyer(dev,scanvalue):
+    """ topic:scanner/code, payload:couleur courante """
+    client.publish(topic="scanner/{}".format(scanvalue), payload="{}".format(scanners[dev].couleur), qos=0, retain=False)
+
+def hoooputain(dev,couleur):
+    """ topic:end, payload:couleur courante passage à la nouvelle couleur """
+    client.publish(topic="end", payload="{}".format(scanners[dev].couleur), qos=0, retain=False)
+    scanners[dev].couleur = couleurs[couleur]
 
 async def publishScan(dev):
-    # read the value scanned by a device and publish a topic on mqtt broker
     scanvalue = ""
     async for ev in dev.async_read_loop():
         if ev.type == ecodes.EV_KEY:
-            # print(evdev.categorize(ev))
+            #print(evdev.categorize(ev))
             data = evdev.categorize(ev)
-            if data.keystate == 1 and data.scancode != 42:
+            if data.keystate ==1 and data.scancode != 42 :
                 if data.scancode == 28:
-                    if scanvalue == "fin d4of":
-                        # TODO payload should be the color associated to the device, not the device itself
-                        client.publish(topic=TOPIC_END,
-                                       payload="{}".format(dev), qos=0, retain=False)
+                    print(scanvalue)
+                    if "couleur" in scanvalue:
+                        hoooputain(dev.phys,scanvalue)
                     else:
-                        # TODO payload should be the color associated to the device, not the device itself
-                        compound = read_compound(scanvalue)
-                        client.publish(topic=TOPIC_SEARCH_BASE + "/{}".format(compound),
-                                       payload="{}".format(dev), qos=0, retain=False)
+                        if scanvalue == "effacer":
+                            envoyerFin(dev.phys)
+                        else:
+                        	envoyer(dev.phys,scanvalue)
                     scanvalue = ""
                 else:
-                    # print(scancodes[data.scancode])
+                    #print(scancodes[data.scancode])
                     scanvalue = scanvalue+scancodes[data.scancode]
     print("fin du scan")
-
-
-def read_compound(barcode):
-    if len(barcode) == 32:
-        return int(barcode[12:18])
-    else:
-        return None
-
 
 loop = asyncio.get_event_loop()
 
 if __name__ == "__main__":
-
-    # create async task for all scanner devices
     devices = getDevice()
     for device in devices:
         asyncio.ensure_future(publishScan(device))
-
-    # execute the callbacks forever
+ 
     loop.run_forever()
-
     client.loop_stop()
--- sources/RPi/sequenceur.py
+++ sources/RPi/sequenceur.py
@@ -1,48 +1,131 @@
-import asyncio
-import os
+#!/usr/bin/python3
+
+from threading import Thread
 import paho.mqtt.client as mqtt
-import finder
-
-# constants
-TOPIC_END = "end"
-TOPIC_SEARCH_BASE = "search"
-TOPIC_ERROR_NO_COMPONENTS = "error/no-components"
-
-find = finder()
+import os
 
 
-def on_connect(client, userdata, flags, rc):
-    print("Connected with result code " + str(rc))
-
-
+broker_url = "localhost"
+#broker_url = "192.168.1.2"
+broker_port = 1883
 def on_message(client, userdata, message):
-    client.publish(topic=TOPIC_END, payload=message.payload,
-                   qos=1, retain=False)
-    etopic = find.explode_topic(message.topic)
-    components = find.search_components(etopic["info"])
-    if size(components) == 0:
-        client.publish(topic=TOPIC_ERROR_NO_COMPONENTS,
-                       payload=message.payload, qos=1, retain=False)
-    else:
-        for component in components:
-            client.publish(topic=TOPIC_SEARCH_BASE + "/{}".format(component),
-                           payload=message.payload, qos=1, retain=False)
+	print("Test_Reçu:{}".format(message.payload))
 
+def receptionMessages():
+	client = mqtt.Client()
+	client.connect(broker_url, broker_port)
+	client.on_message = on_message
+	client.subscribe("scanner/#")
+	client.loop_start()
+	while 1:
+		pass
 
-def start_client():
-    client = mqtt.Client()
-    client.on_connect = on_connect
-    client.on_message = on_message
-    client.connect("localhost", 1883, 60)
-    client.loop_forever()
+	
+def Scan2Ident(scanvalue):
+	"0010566601000 -> 105666"
+	if len(scanvalue) == 13:
+		return scanvalue[2:8]
+	else:
+		return None
+		
+class Machine(Thread):
+	fini = False
+	ident = ""
+	df_idents ={}
+	product_ident = None
 
+	def explode_topic(self, topic):
+		# retrieve type + info contained in a topic
+		ttopic = topic.split(sep="/", maxsplit=1)
+		print("explode_topic",ttopic)
+		if len(ttopic) == 1:
+			return {"base": ttopic[0], "info": ""}
+		else:
+			print({"base": ttopic[0], "info": Scan2Ident(ttopic[1])})
+			return {"base": ttopic[0], "info": Scan2Ident(ttopic[1])}
+			
+	def on_message(self,client, userdata, message):
+		print("Reçu:{}:{}".format(message.topic,message.payload))
+		_id_produit = self.explode_topic(message.topic)["info"]
+		_idents = self.identspour(_id_produit)
+		print(_id_produit, _idents)
+		print("publish: end:{}".format(message.payload))
+		self.client.publish(topic="end", payload=message.payload, qos=0, retain=False)
+		
+		if _idents != None:	
+			for ident in _idents:
+				print("publish: search/{}:{}".format(ident,message.payload))
+				self.client.publish(topic="search/{}".format(ident), payload=message.payload, qos=0, retain=False)
+		
+	def read_csv(self):
+		f = open("idents.csv", "r")
+		s = f.read().split("\n")
+		for line in s[1:-1]:
+			_idents = line.split(",")
+			key = _idents[1]
+			if key in self.df_idents:
+				self.df_idents[key].append(_idents[0])
+			else:	
+				self.df_idents[key] = [_idents[0]]
+		f.close()
 
-async def main():
-    while 1:
-        print("task", end=" ")
-        await  asyncio.sleep(1)
+	def __init__(self,NomIlot="Logistique"):
+		Thread.__init__(self)
+		self.NomIlot = NomIlot
+		self.client = mqtt.Client()
+		self.client.connect(broker_url, broker_port)
+		self.client.on_message = self.on_message
+		self.client.subscribe("scanner/#")
+		
+		self.state = "enCours"
+		self.Etats={"enCours":{"Action":self.enCours,"EtatSuivant":"enCours"}}
+		self.read_csv()#pd.read_csv("idents.csv",encoding='utf-8')
+		
+		self.client.loop_start()
+	
+	def identspour(self,ident_produit):
+		print("recherche:{} dans {}".format(ident_produit,self.df_idents))
+		_idents = self.df_idents[ident_produit]
+		return _idents
+		
+	def enCours(self):
+		pass #Attente d'un scan
+		
 
+	def run(self):
+		while not self.fini:
+			self.Etats[self.state]["Action"]()
+			self.state = self.Etats[self.state]["EtatSuivant"]
+			#self.client.loop() loop_start à la place ?
+		self.client.loop_stop()
+			
+	
+def testIdents():
+	print("idents_pour(566606)",idents_pour(566606))
+	print("idents_pour(566602)",idents_pour(566602))
+	print("idents_pour(563457)",idents_pour(563457))
 
-if __name__ == '__main__':
-    start_client
-    asyncio.run(main())
+def testFunction():
+	receptionMessages()
+def testread_csv():
+	df_idents={}
+	f = open("idents.csv", "r")
+	s = f.read().split("\n")
+	for line in s[1:-1]:
+		_idents = line.split(",")
+		
+		key = _idents[1]
+		if key in df_idents:
+			df_idents[key].append(_idents[0])
+		else:	
+			df_idents[key] = [_idents[0]]
+	print(df_idents["448861"])
+	f.close()
+
+if __name__ == "__main__":
+	#testFunction()
+	#testIdents()
+	#testread_csv()
+	#idents_pour("438132")
+	m=Machine()
+	m.start()
